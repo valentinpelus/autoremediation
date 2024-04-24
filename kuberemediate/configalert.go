e93df8a3d8688cd9138cc5664e176e9efb6bf368
Valentin PELUS <valentinpelus@MacBook-Pro-de-Valentin.local>
2024-04-24 11:36:34 +0200

    fix: merge conflict fixed

---
+++ kuberemediate/configalert.go
@@ -0,0 +1,41 @@
+package kuberemediate
+
+import (
+	"os"
+
+	"github.com/rs/zerolog/log"
+	"gopkg.in/yaml.v2"
+)
+
+type Alert struct {
+	AlertName string `yaml:"alertname"`
+	Enabled   bool   `yaml:"enabled"`
+	Action    string `yaml:"action"`
+}
+
+type AlertList struct {
+	AlertList []Alert `yaml:"alertRulesList"`
+}
+
+var Alerts AlertList
+var EnabledAlertList [][]string
+
+func LoadConfKubeAlert(confPath string) {
+	yamlFile, err := os.ReadFile(confPath)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Can't read kuberemediate alerts config file")
+		os.Exit(1)
+	}
+	err = yaml.Unmarshal(yamlFile, &Alerts)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Unmarshal error on alert list in configuration")
+		os.Exit(1)
+	}
+
+	confAlertList := Alerts.AlertList
+	for i := range confAlertList {
+		if confAlertList[i].Enabled {
+			EnabledAlertList = append(EnabledAlertList, []string{confAlertList[i].AlertName, confAlertList[i].Action})
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	"remediate/kuberemediate"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+var (
+	Client HTTPClient
+)
+
+func main() {
+
+	// Init of zerolog library and config
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	confPath := flag.String("conf", "config.yaml", "Config path")
+	debug := flag.Bool("debug", false, "sets log level to debug")
+	//tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
+	flag.Parse()
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if *debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	// Loading configuration files
+	kuberemediate.LoadConfKube(*confPath)
+	kuberemediate.LoadConfKubeAlert(*confPath)
+
+	// Loading kubeconfig file with context
+	kube_config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// Creates the clientset
+	clientset, err := kubernetes.NewForConfig(kube_config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Init http client
+	Client = &http.Client{}
+
+	// Init AMUrl to allow alerts query
+	jsonUrl := kuberemediate.Conf.QueryURL + "/api/v1/alerts"
+
+	ListSupportedAlert := kuberemediate.EnabledAlertList
+
+	fmt.Println(ListSupportedAlert)
+
+	for {
+		time.Sleep(10 * time.Second)
+		log.Info().Msgf("Check ongoing")
+		// Querying Alertmanager to check if alert is firing for backend size divergence and proceed to deletion if needed
+		alertPodExtractList := kuberemediate.GetVMAlertMatch(jsonUrl, ListSupportedAlert)
+		fmt.Println("Print of return alertPodExtractList : ", alertPodExtractList)
+		for i := range alertPodExtractList {
+			podName := alertPodExtractList[i][0]
+			namespace := alertPodExtractList[i][1]
+			alertAction := alertPodExtractList[i][2]
+			alertName := alertPodExtractList[i][3]
+			if (len(podName) > 0) && (len(namespace) > 0) {
+				log.Info().Msgf("Detecting pod %s in namespace %s on divergence", podName, namespace)
+
+				// Parse returned alertPodExtractList to determine which action should be done with remediate
+				switch alertAction {
+				case "deletePod":
+					log.Info().Msgf("Delete pod %s in namespace %s on divergence", podName, namespace)
+					kuberemediate.DeletePod(podName, clientset, namespace)
+					postMessageSlack(alertName, namespace, confPath)
+				case "enrichAlert":
+					kuberemediate.DescribeDeployment(podName, clientset, namespace)
+
+				}
+			}
+		}
+	}
+}
