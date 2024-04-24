f9cc7aab806b835a2760982f2d64f1093fdcaf01
Valentin Pelus <valentin.pelus@bedrockstreaming.com>
2024-04-24 11:29:58 +0200

    feat(style): reorg code struct (#8)
    Co-authored-by: Valentin PELUS <valentinpelus@MacBook-Pro-de-Valentin.local>

--- notification/slack.go
+++ notification/slack.go
@@ -16,10 +16,16 @@ type SlackClientWebhook struct {
 }
 
 type SlackClient struct {
+<<<<<<< HEAD
 	UserName   string
 	Channel    string
 	TimeOut    time.Duration
 	WebHookUrl string
+=======
+	UserName string
+	Channel  string
+	TimeOut  time.Duration
+>>>>>>> 6335c2f (feat(style): reorg code struct (#8))
 }
 
 type SimpleSlackRequest struct {
